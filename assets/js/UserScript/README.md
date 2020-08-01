92d7e7a8510cb0bc4bbdee5539c3b3a7d6751759
Jay.Young <8641055+Jay-Young@users.noreply.github.com>
2020-08-01 11:22:27 +0800

    update

---
+++ assets/js/UserScript/README.md
@@ -0,0 +1,3 @@
+# 一些油猴脚本
+
+- testv-like-coin.user.js：TESTV点赞投币机，为TESTV发布的视频自动点赞投币，避免遗忘而白嫖。
---
+++ assets/js/UserScript/testv-like-coin.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         TESTV点赞投币机
+// @version      1.0
+// @description  为TESTV发布的视频自动点赞投币，避免遗忘而白嫖
+// @author       铁丝@深表遗憾
+// @match        *://*.bilibili.com/video/*
+// @updateURL    https://cdn.jsdelivr.net/gh/Jay-Young/jsDelivrCDN@master/assets/js/UserScript/testv-like-coin.user.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/Jay-Young/jsDelivrCDN@master/assets/js/UserScript/testv-like-coin.user.js
+// @supportURL   https://github.com/Jay-Young/jsDelivrCDN/issues
+// @homepage     https://github.com/Jay-Young/jsDelivrCDN/tree/master/assets/js/UserScript
+// @grant        none
+// @icon         https://www.bilibili.com/favicon.ico
+// @note         根据兰州小红鸡提供的脚本(http://js.idealli.com/bilibili.js)修改，在此感谢
+// ==/UserScript==
+
+(function() {
+    if (document.getElementsByClassName('username')[0].innerText == 'TESTV官方频道') {
+        var T = 30000; // 如果我为这个视频停留30秒，那么我将投币点赞
+        setTimeout(function(){
+            var likeBtn = document.getElementsByClassName("like")[0];
+            if (likeBtn.className == "like") {
+                likeBtn.click();
+                Toast.info("当前视频没有字幕.","下载字幕",3e3);
+                console.log("已自动为up主点赞");
+            }
+
+            // 获取今日投币情况
+            var httpRequest = new XMLHttpRequest();
+            httpRequest.open('GET', 'https://www.bilibili.com/plus/account/exp.php', true);
+            httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+
+            //获取数据后的处理程序
+            httpRequest.onreadystatechange = function () {
+                if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                    var json = httpRequest.responseText;//获取到json字符串，还需解析
+                    var today = JSON.parse(json).number;
+                    console.log("今日已投币：", today);
+                    document.getElementsByClassName("coin")[0].click();
+                    setTimeout(function(){
+                        document.getElementsByClassName("bi-btn")[0].click();
+                    }, 200);
+                }
+            };
+        }, T)
+    }
+})();
